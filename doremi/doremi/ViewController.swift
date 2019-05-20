bf114bf2099b2a56f16c4b9b50afa28be4a1046e
JeongAKo <kja2683@gmail.com>
2019-05-20 20:44:24 +0900

    teamDoremi first collaboration

---
+++ doremi/doremi/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  doremi
+//
+//  Created by brian은석 on 20/05/2019.
+//  Copyright © 2019 brian. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+
+var soundEffect: AVAudioPlayer?
+
+class ViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func doremiAction(_ sender: UIButton) {
+        a(tag: sender.tag)
+    }
+    
+    
+    
+    
+    
+    func a(tag: Int) {
+        
+        let url = Bundle.main.url(forResource: "dore/\(tag)", withExtension: "mp3")
+        if let url = url{
+            
+            do {
+                soundEffect = try AVAudioPlayer(contentsOf: url)
+                guard let sound = soundEffect else { return }
+                print("success")
+                sound.prepareToPlay()
+                sound.play()
+            } catch let error {
+                print(error.localizedDescription)
+            }
+            
+        }
+    }
+    
+}
+
+
+
