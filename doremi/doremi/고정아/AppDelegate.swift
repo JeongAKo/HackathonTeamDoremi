b95882f44d6a1c768c300dda20f68c7261134b00
JeongAKo <kja2683@gmail.com>
2019-05-21 21:21:42 +0900

    assets 수정

--- doremi/doremi/고정아/AppDelegate.swift
+++ doremi/doremi/고정아/AppDelegate.swift
@@ -10,14 +10,20 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
-
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        
+        window?.backgroundColor = .white
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = LoginViewController()
+        window?.makeKeyAndVisible()
+        
         return true
     }
-
+    
+    
 }
-
