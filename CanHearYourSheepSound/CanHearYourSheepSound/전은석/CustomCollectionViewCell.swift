7324c4a50f9177e157ca8e80f186c3fe53cade94
brianeunseokjeon <qed327@naver.com>
2019-05-24 10:42:23 +0900

    은석1차

---
+++ CanHearYourSheepSound/CanHearYourSheepSound/전은석/CustomCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CustomCollectionViewCell.swift
+//  CanHearYourSheepSound
+//
+//  Created by brian은석 on 24/05/2019.
+//  Copyright © 2019 brian. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var imageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
