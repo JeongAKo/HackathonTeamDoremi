a21d0f33f9446ba4616b6933edaf665df216d09c
brianeunseokjeon <qed327@naver.com>
2019-05-24 10:16:06 +0900

    start

---
+++ CanHearYourSheepSound/CanHearYourSheepSound/고정아/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  CanHearYourSheepSound
+//
+//  Created by brian은석 on 24/05/2019.
+//  Copyright © 2019 brian. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ CanHearYourSheepSound/CanHearYourSheepSound/전은석/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CanHearYourSheepSound
+//
+//  Created by brian은석 on 24/05/2019.
+//  Copyright © 2019 brian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
